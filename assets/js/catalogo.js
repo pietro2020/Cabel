0d99f927fa3fd39411f7c39990759f8658bed193
Pietro <pietrovonovono@gmail.com>
2026-09-10 11:58:52 -0300

    Atualização da pagina de contato e da pagina de catalogo

---
+++ assets/js/catalogo.js
@@ -0,0 +1,29 @@
+async function carregarProdutos() {
+    const response = await fetch('cabos.json');
+    const produtos = await response.json();
+
+    const energiaList = document.getElementById('energia-list');
+    const telecomList = document.getElementById('telecom-list');
+
+    produtos.forEach(produto => {
+        const li = document.createElement('li');
+
+        li.innerHTML = `
+            <a href="produto.html?id=${produto.id}">
+                <img src="img/catalogo/produtos/${produto.id}.png" alt="${produto.titulo}">
+                <div class="card-info">
+                    <span class="card-titulo">${produto.titulo}</span>
+                    <span class="ver-produto">Ver produto →</span>
+                </div>
+            </a>
+        `;
+
+        if (produto.tipo === 'energia') {
+            energiaList.appendChild(li);
+        } else if (produto.tipo === 'telecom') {
+            telecomList.appendChild(li);
+        }
+    });
+}
+
+carregarProdutos();
